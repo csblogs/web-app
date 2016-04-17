ae5b8d4df9ec3be8511277b774374f58e4db6ec8
Rob Crocombe <rjwc1993@yahoo.co.uk>
2016-04-17 13:40:11 -0700

    Server, Babel, and Handlebars now watch for changes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-import express         from 'express';
-import exphbs          from 'express-handlebars';
+import express from 'express';
+import exphbs from 'express-handlebars';
+import path from 'path';
+import log from './log';
 import configureHelmet from './security/configure-helmet';
-import log             from './log';
-import requestLogger   from './log/request-logger';
-import indexRoute      from './routes/index';
-import path            from 'path';
+import requestLogger from './log/request-logger';
+import indexRoute from './routes/index';
 
 const app = express();
 const port = process.env.PORT;
@@ -15,7 +15,9 @@ app.use(requestLogger);
 app.use('/', indexRoute);
 
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
+  defaultLayout: 'main',
+  layoutsDir: 'app/views/layouts/',
+  partialsDir: 'app/views/partials/'
 }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
